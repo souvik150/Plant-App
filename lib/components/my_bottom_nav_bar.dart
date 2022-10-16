f5aaeb4e929c8c161bd28f584a4b3c23502f9ccd
souvik150 <souvikmukherjee150@gmail.com>
2022-10-16 19:55:22 +0530

    feat: home screen

---
+++ lib/components/my_bottom_nav_bar.dart
@@ -0,0 +1,44 @@
+import "package:flutter/material.dart";
+import "package:plant_app/constants.dart";
+import 'package:flutter_svg/flutter_svg.dart';
+
+class MyBottomNavBar extends StatelessWidget {
+  const MyBottomNavBar({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        padding: const EdgeInsets.only(
+            left: kDefaultPadding * 2, right: kDefaultPadding * 2),
+        height: 80,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          boxShadow: [
+            BoxShadow(
+              offset: const Offset(0, -10),
+              blurRadius: 35,
+              color: kPrimaryColor.withOpacity(0.30),
+            ),
+          ],
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            IconButton(
+              onPressed: () {},
+              icon: SvgPicture.asset("assets/icons/flower.svg"),
+            ),
+            IconButton(
+              onPressed: () {},
+              icon: SvgPicture.asset("assets/icons/heart-icon.svg"),
+            ),
+            IconButton(
+              onPressed: () {},
+              icon: SvgPicture.asset("assets/icons/user-icon.svg"),
+            ),
+          ],
+        ));
+  }
+}
--- lib/screens/home/components/body.dart
+++ lib/screens/home/components/body.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
+import 'package:plant_app/screens/home/components/featured_plants.dart';
 import 'package:plant_app/screens/home/components/header_with_searchbox.dart';
 import 'package:plant_app/screens/home/components/recommend_plants.dart';
 import 'package:plant_app/screens/home/components/title_with_more_btn.dart';
@@ -20,42 +21,9 @@ class Body extends StatelessWidget {
           TitleWithMoreBtn(title: "Recommended", press: () {}),
           RecommendPlants(),
           TitleWithMoreBtn(title: "Featured Plants", press: () {}),
-          FeaturePlantCard(
-              image: "assets/images/bottom_img_1.png", press: () {}),
+          FeaturedPlants(),
+          SizedBox(height: kDefaultPadding)
         ],
-      ),
-    );
-  }
-}
-
-class FeaturePlantCard extends StatelessWidget {
-  const FeaturePlantCard({
-    Key? key,
-    required this.image,
-    required this.press,
-  }) : super(key: key);
-  final String image;
-  final VoidCallback press;
-
-  @override
-  Widget build(BuildContext context) {
-    Size size = MediaQuery.of(context).size;
-    return GestureDetector(
-      onTap: press,
-      child: Container(
-        margin: EdgeInsets.only(
-          left: kDefaultPadding,
-          top: kDefaultPadding / 2,
-          bottom: kDefaultPadding / 2,
-        ),
-        width: size.width * 0.8,
-        height: 185,
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(10),
-          image: DecorationImage(
-            image: AssetImage(image),
-          ),
-        ),
       ),
     );
   }
