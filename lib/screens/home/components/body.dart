36c7572410753beb1b6897b2cff2d46382824b60
souvik150 <souvikmukherjee150@gmail.com>
2022-10-16 12:20:48 +0530

    feat: section

--- lib/screens/home/components/body.dart
+++ lib/screens/home/components/body.dart
@@ -2,6 +2,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:plant_app/screens/home/components/header_with_searchbox.dart';
+import 'package:plant_app/constants.dart';
 
 class Body extends StatelessWidget {
   @override
@@ -12,6 +13,29 @@ class Body extends StatelessWidget {
       child: Column(
         children: <Widget>[
           HeaderWithSearchBox(size: size),
+          Container(
+              height: 24,
+              child: Stack(
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.only(left: kDefaultPadding / 4),
+                    child: Text(
+                      "Recommended",
+                      style:
+                          TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  Positioned(
+                      bottom: 0,
+                      left: 0,
+                      right: 0,
+                      child: Container(
+                        margin: EdgeInsets.only(right: kDefaultPadding / 4),
+                        height: 7,
+                        color: kPrimaryColor.withOpacity(0.2),
+                      ))
+                ],
+              ))
         ],
       ),
     );
--- lib/screens/home/components/header_with_searchbox.dart
+++ lib/screens/home/components/header_with_searchbox.dart
@@ -15,6 +15,7 @@ class HeaderWithSearchBox extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+        margin: EdgeInsets.only(bottom: kDefaultPadding * 2.5),
         // It will cover 20% of our total height
         height: size.height * 0.2,
         child: Stack(
