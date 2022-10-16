f8ab2d86bcfff517668fb53f7a1faa854f5d0752
souvik150 <souvikmukherjee150@gmail.com>
2022-10-16 19:05:17 +0530

    feat: recommended section

--- lib/screens/home/components/body.dart
+++ lib/screens/home/components/body.dart
@@ -2,6 +2,9 @@
 
 import 'package:flutter/material.dart';
 import 'package:plant_app/screens/home/components/header_with_searchbox.dart';
+import 'package:plant_app/screens/home/components/recommend_plants.dart';
+import 'package:plant_app/screens/home/components/title_with_more_btn.dart';
+
 import 'package:plant_app/constants.dart';
 
 class Body extends StatelessWidget {
@@ -9,34 +12,50 @@ class Body extends StatelessWidget {
   Widget build(BuildContext context) {
     // It will provide us total height and width of our screen
     Size size = MediaQuery.of(context).size;
+    // Enable scroll on smaller devices
     return SingleChildScrollView(
       child: Column(
         children: <Widget>[
           HeaderWithSearchBox(size: size),
-          Container(
-              height: 24,
-              child: Stack(
-                children: <Widget>[
-                  Padding(
-                    padding: const EdgeInsets.only(left: kDefaultPadding / 4),
-                    child: Text(
-                      "Recommended",
-                      style:
-                          TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                    ),
-                  ),
-                  Positioned(
-                      bottom: 0,
-                      left: 0,
-                      right: 0,
-                      child: Container(
-                        margin: EdgeInsets.only(right: kDefaultPadding / 4),
-                        height: 7,
-                        color: kPrimaryColor.withOpacity(0.2),
-                      ))
-                ],
-              ))
+          TitleWithMoreBtn(title: "Recommended", press: () {}),
+          RecommendPlants(),
+          TitleWithMoreBtn(title: "Featured Plants", press: () {}),
+          FeaturePlantCard(
+              image: "assets/images/bottom_img_1.png", press: () {}),
         ],
+      ),
+    );
+  }
+}
+
+class FeaturePlantCard extends StatelessWidget {
+  const FeaturePlantCard({
+    Key? key,
+    required this.image,
+    required this.press,
+  }) : super(key: key);
+  final String image;
+  final VoidCallback press;
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return GestureDetector(
+      onTap: press,
+      child: Container(
+        margin: EdgeInsets.only(
+          left: kDefaultPadding,
+          top: kDefaultPadding / 2,
+          bottom: kDefaultPadding / 2,
+        ),
+        width: size.width * 0.8,
+        height: 185,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          image: DecorationImage(
+            image: AssetImage(image),
+          ),
+        ),
       ),
     );
   }
---
+++ lib/screens/home/components/recommend_plants.dart
@@ -0,0 +1,119 @@
+import "package:flutter/material.dart";
+import 'package:plant_app/constants.dart';
+
+class RecommendPlants extends StatelessWidget {
+  const RecommendPlants({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      padding: const EdgeInsets.only(right: kDefaultPadding),
+      child: Row(
+        children: [
+          RecommendPlantCard(
+            image: "assets/images/image_1.png",
+            title: "Samantha",
+            country: "Russia",
+            price: 440,
+            press: () {},
+          ),
+          RecommendPlantCard(
+            image: "assets/images/image_2.png",
+            title: "Angelica",
+            country: "Russia",
+            price: 440,
+            press: () {},
+          ),
+          RecommendPlantCard(
+            image: "assets/images/image_3.png",
+            title: "Samantha",
+            country: "Russia",
+            price: 440,
+            press: () {},
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class RecommendPlantCard extends StatelessWidget {
+  const RecommendPlantCard({
+    Key? key,
+    required this.image,
+    required this.title,
+    required this.country,
+    required this.price,
+    required this.press,
+  }) : super(key: key);
+
+  final String image, title, country;
+  final int price;
+  final VoidCallback press;
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Container(
+      margin: const EdgeInsets.only(
+        left: kDefaultPadding,
+        top: kDefaultPadding / 2,
+        bottom: kDefaultPadding * 2.5,
+      ),
+      width: size.width * 0.4,
+      child: Column(
+        children: <Widget>[
+          Image.asset(image),
+          GestureDetector(
+            onTap: press,
+            child: Container(
+                padding: EdgeInsets.all(kDefaultPadding / 2),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: const BorderRadius.only(
+                    bottomLeft: Radius.circular(10),
+                    bottomRight: Radius.circular(10),
+                  ),
+                  boxShadow: [
+                    BoxShadow(
+                      offset: Offset(0, 10),
+                      blurRadius: 50,
+                      color: kPrimaryColor.withOpacity(0.23),
+                    ),
+                  ],
+                ),
+                child: Row(children: <Widget>[
+                  RichText(
+                    text: TextSpan(
+                      children: [
+                        TextSpan(
+                          text: "$title\n".toUpperCase(),
+                          style: Theme.of(context).textTheme.button,
+                        ),
+                        TextSpan(
+                          text: country.toUpperCase(),
+                          style: TextStyle(
+                            color: kPrimaryColor.withOpacity(0.5),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  const Spacer(),
+                  Text(
+                    '\$$price',
+                    style: Theme.of(context)
+                        .textTheme
+                        .button
+                        ?.copyWith(color: kPrimaryColor),
+                  )
+                ])),
+          ),
+        ],
+      ),
+    );
+  }
+}
